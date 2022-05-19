9d88a072c8151bbbd4e2bc975dcc14181d7d35be
aadityas201 <69757025+aadityas201@users.noreply.github.com>
2022-05-19 12:08:19 +0530

    Login and  Registeration

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 function App() {
   return (
     <div className="App">
-      <Profile />
+      <Register />
     </div>
   );
 }
