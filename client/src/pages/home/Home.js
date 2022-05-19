cdfe48eca9fafe1b839e41ea84c59dc529667f97
aadityas201 <69757025+aadityas201@users.noreply.github.com>
2022-05-19 11:35:48 +0530

    Main feed

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Leftbar from "../../components/leftbar/Leftbar";
+import Feed from "../../components/mainfeed/Feed";
+import "./home.css";
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <Leftbar />
+      <div className="homeContainer">
+        <Leftbar />
+        <Feed />
+      </div>
     </div>
   );
 };
